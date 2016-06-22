68bec0863da89869f2ece8f68e5dd08d45249650
Thibaut Moiroud <thibaut.moiroud@kozea.fr>
2016-06-22 11:44:19 +0200

    Resolve #55 and change an id.

--- static/js/kozea.js
+++ static/js/kozea.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
 
     $(window).scroll(function(){
 		if ($(this).scrollTop() > 500) {
-			$('#backToTop').fadeIn(300);
+			$('#back-to-top').fadeIn(400);
 		} else {
-			$('#backToTop').fadeOut(300);
+			$('#back-to-top').fadeOut(400);
 		}
 	});
 
-	$('#backToTop').on('click', function() {
-		$('html, body').animate({scrollTop : 0}, 1000);
+	$('#back-to-top').on('click', function() {
+		$('html, body').animate({scrollTop : 0}, 900);
 	});
+
+    $('#go-to-about').on('click', function() {
+        $('html, body').animate({scrollTop : $($.attr(this, 'href')).offset().top}, 900);
+    });
 });
