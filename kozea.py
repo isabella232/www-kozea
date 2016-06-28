711c6f80dacdeb9e388ffa62a19223d5a259106f
Thibaut Moiroud <thibaut.moiroud@kozea.fr>
2016-06-28 17:27:06 +0200

    Now access_token is stored in config (it may be an issue later) which provide the user to see instagram's media.

--- kozea.py
+++ kozea.py
@@ -10,41 +10,34 @@ app.config.from_envvar('KOZEA_SETTINGS')
 CLIENT_ID = app.config['CLIENT_ID']
 CLIENT_SECRET = app.config['CLIENT_SECRET']
 REDIRECT_URI = app.config['REDIRECT_URI']
-
-
-@app.route('/<page>')
-def page(page='index'):
-    recorded_pages = ['index', '404', 'about', 'activity', 'contact',
-        'expertise', 'legal', 'references']
-    if page in recorded_pages:
-        return render_template('{}.html'.format(page), page=page)
-    else:
-        return render_template('404.html')
+USER_ID = app.config['USER_ID']
+ACCESS_TOKEN = app.config['ACCESS_TOKEN']
 
 @app.route('/')
-@app.route('/index')
-def index():
-    instaAPI = InstagramAPI(client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
-    try:
-        access_token = instaAPI.exchange_code_for_access_token(
-            request.args.get('code'))[0]
-    except OAuth2AuthExchangeError:
-        redirect_uri = instaAPI.get_authorize_login_url(
-            scope = ['public_content'])
-        return redirect(redirect_uri)
-    instaAPI = InstagramAPI(client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET, access_token=access_token)
-    recent_media, next_ = instaAPI.user_recent_media(
-        user_id=instaAPI.user().id, count=3)
+@app.route('/<page>')
+def page(page='index'):
+    recorded_pages = [
+        'index', 'about', 'activity', 'contact',
+        'expertise', 'legal', 'references']
+    if page in recorded_pages:
+        if page == 'index':
+            return get_insta_media()
+        return render_template('{}.html'.format(page), page=page)
+    return render_template('404.html')
+
+def get_insta_media():
+    instaAPI = InstagramAPI(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN)
+    recent_media, next_ = instaAPI.user_recent_media(user_id=USER_ID, count=3)
     render_insta = []
     for media in recent_media:
         render_insta.append({
             'link': media.link,
             'src': media.get_low_resolution_url(),
             'title': media.caption.text})
-    return render_template('index.html', page='index',
-        render_insta=render_insta)
+    return render_template(
+        'index.html', page='index', render_insta=render_insta)
 
 
 if __name__ == '__main__':
