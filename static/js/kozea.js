4f3ea34e81db55255f838721644ddb7f24672875
Clément Plasse <clement.plasse@kozea.fr>
2016-07-07 10:36:33 +0200

    Popup for sent message

--- static/js/kozea.js
+++ static/js/kozea.js
@@ -15,4 +15,20 @@ $(document).ready(function() {
   $('#go-to-about').on('click', function() {
     $('html, body').animate({scrollTop : $($.attr(this, 'href')).offset().top}, 900);
   });
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    $.post($(e.target).attr('action'), $(e.target).serialize())
+      .done(function() {
+        var $popup = $('<div class="popup">').append('<p>Message envoyé.</p>')
+        $('body').append($popup, $('<div class="black-overlay">'));
+
+        function remove_popup() {
+          $popup.fadeOut(400, function() {
+            $(".popup, .black-overlay").remove();
+          });
+        }
+        setTimeout(remove_popup, 2000);
+      });
+  });
 });
