9a32197677063d457f8f8800477a46b4a6a6160c
Julien Sanchez <julien.sanchez@kozea.fr>
2021-07-15 16:22:04 +0200

    Add product cards to solutions page

--- www_kozea/__init__.py
+++ www_kozea/__init__.py
@@ -28,8 +28,73 @@ def create_app():
         "témoignages",
     ]
 
+    page_data = {
+        "solutions": {
+            "cards": [
+                {
+                    "cover": "images/pharminfo-mini-solutions.png",
+                    "headline": "Pharminfo.fr",
+                    "title": "Votre pharmacie disponible 24h/24 et 7j/7",
+                    "body": "Des services adaptés pour vous et vos patients :",
+                    "features": [
+                        "Envoi d'ordonnance",
+                        "Click and Collect",
+                        "Vente en ligne",
+                        "Prise de rendez-vous",
+                    ],
+                },
+                {
+                    "cover": "images/kozea-media-mini-solutions.png",
+                    "headline": "Kozea media",
+                    "title": "Votre communication maîtrisée et externalisée",
+                    "body": "Kozea media conseille depuis 10 ans les acteurs "
+                    "de santé dans leur transformation digitale.",
+                    "features": [
+                        "Gestion des réseaux sociaux",
+                        "Régie média (publicité)",
+                        "Création graphique et contenu rédactionnel",
+                    ],
+                },
+                {
+                    "cover": "images/backoffice-mini-solutions.png",
+                    "headline": "Backoffice",
+                    "title": "Votre tiers-payant géré de A à Z",
+                    "body": "Redonnez de la valeur à votre métier de "
+                    "pharmacien et gagnez du temps sur vos tâches "
+                    "administratives !",
+                    "features": [
+                        "Externalisation du tiers-payant",
+                        "Formations adaptées à l'équipe officinale",
+                        "Suivi et contrôle en toute autonomie",
+                    ],
+                },
+                {
+                    "cover": "images/promomaker-mini-solutions.png",
+                    "headline": "Promomaker",
+                    "title": "Vos campagnes promotionnelles en 1 clic",
+                    "body": "Créez et diffusez très simplement des campagnes "
+                    "de communication et de promotion pour votre pharmacie.",
+                    "features": [
+                        "Création automatique",
+                        "Accessible pour tous et pour votre groupement",
+                        "Choix du format d'impression",
+                    ],
+                },
+                {
+                    "cover": "images/kozea-community-mini-solutions.png",
+                    "headline": "Kozea community",
+                    "title": "Développement de projets innovants",
+                    "body": "Bénéficiez des logiciels libres développés par "
+                    "les membres de l'équipe Kozea group et de leur expertise "
+                    "pour la création de nouveaux projets.",
+                    "features": [],
+                },
+            ]
+        }
+    }
+
     for page in page_list:
-        create_endpoint(app, page)
+        create_endpoint(app, page, page_data)
 
     @app.route("/")
     def home():
@@ -38,9 +103,9 @@ def create_app():
     return app
 
 
-def create_endpoint(app, page):
+def create_endpoint(app, page, page_data):
     @app.route(f"/{page}/", endpoint=page)
     def view_func():
-        return render_template(f"{page}.html")
+        return render_template(f"{page}.html", data=page_data.get(page))
 
     return view_func
