1bda4c79017b5972343f6e4a0ac757fd5cb10533
Ibtihal Aabdi <ibtihal.aabdi@kozea.fr>
2021-12-27 10:00:28 +0100

    Edit error messages

--- www_kozea/error.py
+++ www_kozea/error.py
@@ -1,9 +1,9 @@
 class NoDateInArticleError(Exception):
     """Exception raised for no date in frontmatter article."""
 
-    def __init__(self, article_title, message=" article has no date."):
+    def __init__(self, article_title, message="article has no date."):
         self.article_title = article_title
-        self.message = "%s %s" % (article_title, message)
+        self.message = f"{article_title} {message}"
         super().__init__(self.message)
 
 
@@ -14,17 +14,17 @@ class TypeDateInArticleError(Exception):
         self,
         article_title,
         message="article does not have the correct format "
-        "for the date. The correcte format is YYYY-MM-DD.",
+        "for the date. Correct format is YYYY-MM-DD.",
     ):
         self.article_title = article_title
-        self.message = "%s %s" % (article_title, message)
+        self.message = f"{article_title} {message}"
         super().__init__(self.message)
 
 
 class FrontmatterError(Exception):
     """Exception raised for error in frontmatter article."""
 
-    def __init__(self, article_title, message=" article has no frontmatter."):
+    def __init__(self, article_title, message="article has no frontmatter."):
         self.article_title = article_title
-        self.message = "%s %s" % (article_title, message)
+        self.message = f"{article_title} {message}"
         super().__init__(self.message)
